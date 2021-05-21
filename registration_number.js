aff926f40b99d07378dd3834a633d95dab5373b1
jodies383 <jodies383@gmail.com>
2021-05-21 16:45:20 +0200

    Changes

--- registration_number.js
+++ registration_number.js
@@ -8,55 +8,65 @@ var resetBtn = document.querySelector(".reset")
 
 var errorMessage = document.querySelector(".errorMessage")
 
+var showBtn = document.querySelector(".show")
 
-addBtn.addEventListener('click', myReg);
 addBtn.addEventListener('click', listReg);
+//showBtn = addEventListener('click', towns)
 
-var existingReg;
+//var existingReg;
 
 if (localStorage['regList']) {
-    existingReg = JSON.parse(localStorage.getItem('regList'));
+    JSON.parse(localStorage.getItem('regList'));
 }
 
-var regEx = /^[A-Z]{1,3}-[A-Z]{1,2}-[0-9]{1,4}$/;
+var regEx = /^[A-Z]{2,3} [0-9]{3}-[0-9]{0,3}$/
+
+var ul = document.getElementById("list");
 
 
 function listReg() {
-    var ul = document.getElementById("list");
-    var li = document.createElement("ul");
+    var li = document.createElement("object");
     li.style.backgroundColor = '#f5b342';
     li.style.borderStyle = "solid";
     li.style.borderColor = 'black';
     li.style.borderRadius = '4px';
-    li.style.maxWidth = '15%';
-    li.style.marginLeft = '38%';
     li.style.fontWeight = 'bold';
     li.style.textAlign = 'center';
 
-if (addRegNum.value){
-    li.appendChild(document.createTextNode(addRegNum.value));
-    ul.appendChild(li);
+    if (addRegNum.value && regEx.test(addRegNum.value)) {
+        li.appendChild(document.createTextNode(addRegNum.value));
+        ul.appendChild(li);
+        localStorage.setItem('regList', JSON.stringify(addRegNum.value));
 
-} else {
-    (errorMessage.innerHTML = "Please enter a registration number")
+    } else {
+        (errorMessage.innerHTML = "Please enter a registration number")
+    }
+  
+
+    setTimeout(function () {
+        errorMessage.innerHTML = "";
+    }, 2000);
 }
-setTimeout(function(){   errorMessage.innerHTML = "";
-}, 2000);
+function towns(){
+    var checkedRadioBtn = document.querySelector("input[name='towns']:checked").value;
+
+for (var i = 0; i < li.length; i++)
+var loopLi = li[i];
+if (checkedRadioBtn === "Cape Town"){
+    return loopLi.startsWith("CY")
+}else if (checkedRadioBtn === "Paarl"){
+    return loopLi.startsWith("CJ")
+}else if (checkedRadioBtn === "George"){
+    return loopLi.startsWith("CAW")
 }
 
+}
 
-let regFunction = registration(existingReg);
 
 function myReg() {
     regFunction.enterRegNum(addRegNum.value)
-    // displayRegNum.innerHTML = regFunction.listReg();
+
 }
-
-
-
-
-
-
 
 
 
--- registration_number_factory.js
+++ registration_number_factory.js
@@ -1,35 +1,8 @@
-function registration(existingReg) {
+function registration() {
 
-    var regList = existingReg || []
-
-    function enterRegNum(regNum) {
-        if (!regList.includes(regNum)) {
-            regList.push(regNum)
-        }
-        localStorage.setItem('regList', JSON.stringify(regList));
-
-    }
-
-    function towns(radioBtn) {
-        if (radioBtn === "Cape Town"){
-            return
-        }
-}
-
-
-
-
-
-
-
-
-    // function listReg() {
-    //     return regList;
-    // }
 
 
     return {
-        enterRegNum,
-        listReg
+      
     }
 }
